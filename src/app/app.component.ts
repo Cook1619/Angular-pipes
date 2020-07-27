7b68958871339efe23a94a5aaab90fa411094102
Cook1619 <cook6348@gmail.com>
2020-07-27 15:44:01 -0500

    Adding boostrap and using titlecase pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipe-app';
+  name: string;
   onNameChange(value: string){
-    console.log(value)
+    this.name = value
   }
 }
